2bbde834e5510106a31ba974ddf75ffcaa63768d
bookking3000 <15048135+bookking3000@users.noreply.github.com>
2025-01-06 09:23:24 +0100

    Fix update script of yealink-usb-connect

--- yealink-usb-connect/tools/chocolateyinstall.ps1
+++ yealink-usb-connect/tools/chocolateyinstall.ps1
@@ -1,17 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.yealink.com/website-service/download/yealink-usb-connect-4.38.33.0.msi' 
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = $url
-  url64bit      = $url64
+  url           = 'https://www.yealink.com/website-service/download/Yealink-USB-Connect-4.39.12.0.msi' 
   softwareName  = 'Yealink USB Connect*'
 
-  checksum      = 'a63b8356bb1874de3410a75aa41b428064dda35c9f72a4c8c83840750f68165b'
+  checksum      = ''
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
