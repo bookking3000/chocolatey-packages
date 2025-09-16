6d92660574eadff86e8650e8be5b856c753f6549
bookking3000 <bookking3000@users.noreply.github.com>
2025-09-16 01:53:16 +0000

    Apply Choco package update - linkus-desktop-client

--- linkus-desktop-client/tools/chocolateyinstall.ps1
+++ linkus-desktop-client/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   url           = 'https://image.yeastar.com/Yeastardownload/linkus_desktop_windows_msi_version.msi'
   softwareName  = 'Linkus Desktop Client*'
-  checksum      = 'e8c01fd553ef0c5f1d055405d30c0b96ef69333a8810fb86c8ef4521b2fdc30c'
+  checksum      = 'fcf3795c74fb572dda7b0c555e88a01fa01562509b78622b48fc23005f74815e'
   checksumType  = 'sha256' 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
