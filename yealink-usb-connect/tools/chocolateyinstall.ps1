2b7d82f1f8352da50377140a264227a4b8c0ad43
bookking3000 <bookking3000@users.noreply.github.com>
2026-02-12 02:55:54 +0000

    Apply Choco package update - yealink-usb-connect

--- yealink-usb-connect/tools/chocolateyinstall.ps1
+++ yealink-usb-connect/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = 'https://www.yealink.com/website-service/download/Yealink-USB-Connect(X86)-4.41.12.0.msi' 
+  url           = 'https://www.yealink.com/website-service/download/Yealink-USB-Connect(X86)-4.41.22.0.msi' 
   softwareName  = 'Yealink USB Connect*'
 
-  checksum      = '6849eff3d75c815f0b00480497602ef59f4e1c94e09de5f679ab3dd0d753d295'
+  checksum      = 'f18d7942c8d5bc3d155b15d7f0051170ccfc20d3a5af0e6a5f5bea3bc2fd5f09'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
